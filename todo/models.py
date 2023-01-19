7e8d2b17be7ea8b47ba5b4f0283cf5e8de5eef6b
Your Name <you@example.com>
2023-01-19 10:59:05 +0900

    created_atを自動入力にした

--- todo/models.py
+++ todo/models.py
@@ -5,4 +5,4 @@ class Todo(models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=50)
     memo =  models.CharField(max_length=200)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
