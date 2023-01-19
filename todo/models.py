fd07aaeb7b83a953ae91f86b167a47f733d6474b
Your Name <you@example.com>
2023-01-19 17:09:13 +0900

    管理画面の見栄え調整

--- todo/models.py
+++ todo/models.py
@@ -6,3 +6,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=50)
     memo =  models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
