c6a3329b4b92878a3b2f02bc01dcd1da7ef7c829
Your Name <you@example.com>
2023-01-19 10:47:04 +0900

    URL修正

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -20,7 +20,7 @@ from rest_framework import routers
 from todo import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.TodoViewSet)
+router.register(r'', views.TodoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 
