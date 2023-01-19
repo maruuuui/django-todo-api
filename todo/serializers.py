883515b87c39095e5bb75af46de7d1b1ff994c6d
Your Name <you@example.com>
2023-01-19 10:34:10 +0900

    View作成

---
+++ todo/serializers.py
@@ -0,0 +1,8 @@
+from todo.models import Todo
+from rest_framework import serializers
+
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'title', 'memo', 'created_at']
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
