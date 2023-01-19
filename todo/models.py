d82edac3abc0ef578da6fe05a8e857b1544418fc
Your Name <you@example.com>
2023-01-19 10:00:43 +0900

    Todoモデル作成

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    memo =  models.CharField(max_length=200)
+    created_at = models.DateTimeField()
