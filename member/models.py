ba3de70fd4149f4b96bbbc9bfc2d9408c871aa6c
Your Name <you@example.com>
2023-01-30 10:36:31 +0900

    案件詳細のオプション変更

--- member/models.py
+++ member/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Member(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField("氏名",max_length=20)
+    name = models.CharField("氏名", max_length=20)
 
     def __str__(self) -> str:
         return self.name
@@ -12,10 +12,10 @@ class Member(models.Model):
 class ProjectRecord(models.Model):
     id = models.BigAutoField(primary_key=True)
     member = models.ForeignKey(Member, verbose_name="社員", on_delete=models.CASCADE)
-    start_date = models.DateField("開始日",null=True)
-    end_date = models.DateField("終了日",null=True)
-    project_abstract = models.CharField("案件概要",max_length=50)
-    project_detail = models.CharField("案件詳細",max_length=1000)
+    start_date = models.DateField("開始日", null=True)
+    end_date = models.DateField("終了日", null=True)
+    project_abstract = models.CharField("案件概要", max_length=50)
+    project_detail = models.CharField("案件詳細", max_length=1000, blank=True)
 
     def __str__(self) -> str:
         return self.project_abstract
--- member/views.py
+++ member/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
-from .models import Member ,ProjectRecord
-from .serializers import MemberSerializer,ProjectRecordSerializer
+from .models import Member, ProjectRecord
+from .serializers import MemberSerializer, ProjectRecordSerializer
 
 
 class MemberViewSet(viewsets.ModelViewSet):
@@ -12,3 +13,14 @@ class MemberViewSet(viewsets.ModelViewSet):
 class ProjectRecordViewSet(viewsets.ModelViewSet):
     queryset = ProjectRecord.objects.all()
     serializer_class = ProjectRecordSerializer
+
+    # POST
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            print(serializer.errors)
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
