edb373cd4d9612e14d472d75f29aeae293065a76
Your Name <you@example.com>
2023-01-30 10:36:40 +0900

    format

--- member/serializers.py
+++ member/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 
-from member.models import Member,ProjectRecord
+from member.models import Member, ProjectRecord
 
 
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields = ["id", "name",]
+        fields = [
+            "id",
+            "name",
+        ]
+
 
 class ProjectRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectRecord
-        fields = ["id", "start_date","end_date","project_abstract","project_detail", "member",]
+        fields = [
+            "id",
+            "start_date",
+            "end_date",
+            "project_abstract",
+            "project_detail",
+            "member",
+        ]
--- todo_api/urls.py
+++ todo_api/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from todo import views
 from member import views as member_views
+from todo import views
 
 router = routers.DefaultRouter()
 router.register(r"member", member_views.MemberViewSet)
